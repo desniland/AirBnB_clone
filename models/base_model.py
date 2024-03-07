bdd3aa1c15766beef159bab9a3c24037b4406c14
desniland <albertdesmond26@gmail.com>
2024-03-07 11:40:26 +0300

    My AirBnB Console

---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""Defines a base model class."""
+import uuid
+import models
+from datetime import datetime
+
+
+class BaseModel:
+    """Base Model"""
+    def __init__(self, *args, **kwargs):
+        """ Constructor """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())  # generate a unique id
+            self.created_at = datetime.now()  # date/time when is created
+            self.updated_at = datetime.now()  # date when it's updated
+            models.storage.new(self)
+
+    def __str__(self):
+        """ prints  __str__ method """
+        className = self.__class__.__name__
+        return "[{}] ({}) {}".format(className, self.id, self.__dict__)
+
+    def save(self):
+        """updates update_at with the current datetime """
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        '''returns a dictionary with all keys/value of the instance'''
+        dict_copy = self.__dict__.copy()
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        dict_copy['__class__'] = self.__class__.__name__
+        return dict_copy
